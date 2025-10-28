55da1e99d52bb2373a706d405357e9035e4fb9db
Axelcichello <csaxel49@gmail.com>
2025-10-28 20:41:28 -0300

    metodo para traer el worker

--- src/routes/worker.routes.js
+++ src/routes/worker.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { uploadWorkerPhoto } from '../middlewares/images/uploadWorkerPhoto.js';
-import { createWorker, getServicesByCategory } from '../controllers/worker.controller.js';
+import { createWorker, getServicesByCategory, getWorkerById } from '../controllers/worker.controller.js';
 
 export const workerRouter = Router();
 
 workerRouter.post('/', uploadWorkerPhoto, createWorker);
 workerRouter.post('/solicitudes-rubro/:id', getServicesByCategory);
+workerRouter.get('/traer-worker/:id', getWorkerById);
 
 
 export default workerRouter;
