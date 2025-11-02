bfb81e7628a1ffef0faece6756597bfb7bdc1d31
Axelcichello <csaxel49@gmail.com>
2025-11-02 09:17:27 -0300

    ruta y metodo para cancelar un acuerdo mutuo

--- src/routes/job_request.routes.js
+++ src/routes/job_request.routes.js
@@ -8,7 +8,8 @@ import {
     checkPostulation,
     getAplicationsByJobRequestId,
     getAplicationById,
-    setMutualAgreement
+    setMutualAgreement,
+    setCancelMutualAgreement
 
 } from "../controllers/job_request.controller.js";
 import { uploadJobRequestPhotos } from '../middlewares/images/updateJobRequestPhotos.js';
@@ -25,6 +26,7 @@ jobRequestRouter.post("/postularse", setPostulation);
 jobRequestRouter.get("/postulaciones-workers/:id", getAplicationsByJobRequestId); // Obtener las postulaciones de una solicitud de servicio
 jobRequestRouter.get("/postulacion-worker/:id", getAplicationById); // Obtener una postulacion por su id
 jobRequestRouter.put("/acuerdo-mutuo/:id", setMutualAgreement); //Evalua la entidad que acepta el acuerdo y pone true la columna de agreement
+jobRequestRouter.put("/cancelar-acuerdo-mutuo/:id", setCancelMutualAgreement); //Evalua la entidad que acepta el acuerdo y pone true la columna de agreement
 jobRequestRouter.get("/:id", validateSchema(idSchema, "params"), getJobRequestsByUserId);
 
 
