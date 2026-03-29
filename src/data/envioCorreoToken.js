d4d597d6ae065405ab43df2cf238029f29c7261c
Axel Cichello <axel.cichello@davinci.edu.ar>
2026-03-29 17:53:25 -0300

    reutilizo archivo para correos de token

--- src/data/envioCorreoToken.js
+++ src/data/envioCorreoToken.js
@@ -2,10 +2,22 @@ import { generateToken } from "../utils/jwt.js";
 import { buildConfirUrl } from "../utils/url.js";
 import { transporter } from "../utils/mailer.js";
 import {getCorreoDeBienvenida} from "../utils/emailTemplates.js";
+import {guardarTokenRegistro} from "./funcionesBBDD/guardarTokenRegistro.js";
+import {prisma} from "./prisma.js";
+import {reescribirTokenRegistro} from "./funcionesBBDD/reescribirTokenRegistro.js";
 
-export const envioCorreoToken = async ({ id, email, nombre }, duracionToken) => {
+export const envioCorreoToken = async ({ id, email, nombre, motivo }, duracionToken) => {
     const token = generateToken({ userId: id }, duracionToken);
     const confirmUrl = buildConfirUrl(token);
+
+    if (token) {
+        if(motivo === "registro"){
+            guardarTokenRegistro(token, id);
+
+        }else if(motivo === "reenvio"){
+            reescribirTokenRegistro(token, id)
+        }
+    }
 
     await transporter.sendMail({
         from: "TRABAJITO APP",
@@ -13,6 +25,8 @@ export const envioCorreoToken = async ({ id, email, nombre }, duracionToken) => 
         subject: "Confirma tu correo y accede a todas las soluciones",
         html: getCorreoDeBienvenida(nombre, confirmUrl),
     });
+
+
 
     // ahora devuelvo ambas cosas
     return { token, confirmUrl };
